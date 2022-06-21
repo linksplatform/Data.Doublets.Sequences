1930f04eca3d3d2144c515ebff1df1fd7825ed2a
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-21 15:18:09 +0600

    Add byte converters draft

--- csharp/Platform.Data.Doublets.Sequences/Numbers/Byte/RawSequenceToByteListConverter.cs
+++ csharp/Platform.Data.Doublets.Sequences/Numbers/Byte/RawSequenceToByteListConverter.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using System.Numerics;
 using Platform.Collections.Stacks;
 using Platform.Converters;
+using Platform.Data.Doublets.CriterionMatchers;
 using Platform.Data.Doublets.Decorators;
+using Platform.Data.Doublets.Sequences.Converters;
 using Platform.Data.Doublets.Sequences.Walkers;
+using Platform.Data.Doublets.Unicode;
+using Platform.Data.Numbers.Raw;
 using Platform.Numbers;
 using Platform.Reflection;
 using Platform.Unsafe;
@@ -15,6 +19,8 @@ namespace Platform.Data.Doublets.Sequences.Numbers.Byte;
 
 public class RawSequenceToByteListConverter<TLinkAddress> : LinksDecoratorBase<TLinkAddress>, IConverter<TLinkAddress, IList<byte>> where TLinkAddress : struct
 {
+    public static readonly EqualityComparer<TLinkAddress> EqualityComparer = EqualityComparer<TLinkAddress>.Default;
+
     public static readonly TLinkAddress MaximumValue = NumericType<TLinkAddress>.MaxValue;
 
     public static readonly TLinkAddress BitMask = Bit.ShiftRight(MaximumValue, 1);
@@ -23,36 +29,80 @@ public class RawSequenceToByteListConverter<TLinkAddress> : LinksDecoratorBase<T
 
     public readonly IConverter<IList<TLinkAddress>, TLinkAddress> ListToSequenceConverter;
 
+    public readonly IConverter<string, TLinkAddress> StringToUnicodeSequenceConverteer;
+    
+    public readonly IConverter<TLinkAddress, string> UnicodeSequenceToStringConverteer;
 
-    public RawSequenceToByteListConverter(ILinks<TLinkAddress> links, IConverter<TLinkAddress> numberToAddressConverter, IConverter<IList<TLinkAddress>,TLinkAddress> listToSequenceConverter) : base(links)
+    public readonly BalancedVariantConverter<TLinkAddress> BalancedVariantConverter;
+    
+
+
+    public RawSequenceToByteListConverter(ILinks<TLinkAddress> links, IConverter<TLinkAddress> numberToAddressConverter, IConverter<IList<TLinkAddress>,TLinkAddress> listToSequenceConverter, StringToUnicodeSequenceConverter<TLinkAddress> stringToUnicodeSequenceConverter) : base(links)
     {
         NumberToAddressConverter = numberToAddressConverter;
         ListToSequenceConverter = listToSequenceConverter;
+        BalancedVariantConverter = new BalancedVariantConverter<TLinkAddress>(links);
+        StringToUnicodeSequenceConverteer = stringToUnicodeSequenceConverter;
+    }
+
+    private void EnsureIsByteArrayLength(TLinkAddress byteArrayLengthAddress)
+    {
+        var source = _links.GetSource(byteArrayLengthAddress);
+        TLinkAddress zero = default;
+        var type = Arithmetic.Increment(zero);
+        var byteArrayLengthType = _links.SearchOrDefault(type, StringToUnicodeSequenceConverteer.Convert("ByteArrayLengthType"));
+        if (EqualityComparer.Equals(byteArrayLengthType, default))
+        {
+            throw new Exception("Could not find ByteArrayLengthType");
+        }
+        if (!EqualityComparer.Equals(source, byteArrayLengthType))
+        {
+            throw new Exception("Source must be ByteArrayLengthType");
+        }
+    }
+
+    private int GetByteArrayLength(TLinkAddress byteArrayLengthAddress)
+    {
+        EnsureIsByteArrayLength(byteArrayLengthAddress);
+        var target = _links.GetTarget(byteArrayLengthAddress);
+        CheckedConverter<TLinkAddress, int> checkedConverter = CheckedConverter<TLinkAddress, int>.Default;
+        return checkedConverter.Convert(NumberToAddressConverter.Convert(target));
+    }
+
+    private byte GetByteFromBitArray(BitArray bitArray ) 
+    {
+        byte[] currentByte = new byte[1];
+        bitArray.CopyTo(currentByte, 0);
+        return currentByte[0];
     }
 
     public IList<byte> Convert(TLinkAddress source)
     {
-        List<byte> byteList = new();
+        var byteArrayLengthAddress = _links.GetSource(source);
+        var byteArrayLength = GetByteArrayLength(byteArrayLengthAddress);
+        List<byte> byteList = new(byteArrayLength);
+        List<bool> currentByteBitList = new(8);
         RightSequenceWalker<TLinkAddress> rightSequenceWalker = new(_links, new DefaultStack<TLinkAddress>());
-        var sequence = rightSequenceWalker.Walk(source);
+        var sequenceAddress = _links.GetTarget(source);
+        var sequence = rightSequenceWalker.Walk(sequenceAddress);
+        CheckedConverter<TLinkAddress, byte> checkedConverter = CheckedConverter<TLinkAddress, byte>.Default;
         foreach (var currentByteAddress in sequence)
         {
-            var currentByte = (byte)(object)NumberToAddressConverter.Convert(currentByteAddress);
-            
-        }
-        var currentByteArray = new ArraySegment<byte>(byteArray);
-        var offset = 0;
-        while (currentByteArray.Array != null)
-        {
-            if (offset != 0)
+            var a = NumberToAddressConverter.Convert(currentByteAddress);
+            var bitArray = new BitArray(a.ToBytes());
+            for (var i = 0; i < bitArray.Count - 1 && i <= byteArrayLength * 8; i++)
             {
-                currentByteArray = new ArraySegment<byte>(byteArray, offset, byteArray.Length - offset);
+                if (currentByteBitList.Count == 8)
+                {
+                    var currentByteBitArray = new BitArray(currentByteBitList.ToArray());
+                    var currentByte = GetByteFromBitArray(currentByteBitArray);
+                    byteList.Add(currentByte);
+                    currentByteBitList.Clear();
+                }
+                var currentBit = bitArray[i];
+                currentByteBitList.Add(currentBit);
             }
-            var byteArrayWithBitMask = Bit.And(currentByteArray.Array.ToStructure<TLinkAddress>(), BitMask);
-            var rawNumber = NumberToAddressConverter.Convert(byteArrayWithBitMask);
-            rawByteAddressList.Add(rawNumber);
-            offset += NumericType<TLinkAddress>.BitsSize - 1;
         }
-        return ListToSequenceConverter.Convert(rawByteAddressList);
+        return byteList;
     }
 }
